000bf8b28760647b28e6e47e55faa556c6df4a6b
pbwsantu-collab <pbwsantu@gmail.com>
2026-09-15 22:41:26 +0530

    Add service worker for offline PWA support

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'grammar-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './js/questions.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
